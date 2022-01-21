fa0e9aad64c9eb002bb195c71e5dfe01b67493c4
Anton Kolesnikov <toni_wheel@inbox.ru>
2022-01-21 13:12:46 +0500

    v1.0

---
+++ app.js
@@ -0,0 +1,12 @@
+const body = document.body;
+const click = document.querySelector("#click");
+
+click.addEventListener("click", isChecked);
+
+function isChecked() {
+  if (click.checked) {
+    body.classList.add("lock");
+  } else {
+    body.classList.remove("lock");
+  }
+}
